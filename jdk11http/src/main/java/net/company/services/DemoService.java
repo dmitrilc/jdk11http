29a1538ed3ad5ccd150b79926b196389f2382267
dmitrilc <dmitrilc@users.noreply.github.com>
2021-07-28 09:50:33 -0400

    Refactor
    The logic of sending requests are moved into this class based on code
    review

--- jdk11http/src/main/java/net/company/services/DemoService.java
+++ jdk11http/src/main/java/net/company/services/DemoService.java
@@ -1,10 +1,15 @@
 package net.company.services;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.JsonMappingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
 
 import net.company.pojos.CatFact;
+
+import static net.company.services.JsonUtils.*;
 
 /**
  * A service class that performs processing for business logic.
@@ -14,24 +19,42 @@ import net.company.pojos.CatFact;
 public class DemoService {
 	
 	/**
-	 * The ObjectMapper instance should be re-used because it is expensive
-	 * to create. If a method needs a configuration that requires a specialized
-	 * ObjectMapper, the method can use ObjectMapper's copy constructor. 
+	 * Each controller will have a httpClient attached to it. the instance
+	 * must be instantiated manually because the project is not using
+	 * dependency injection.
 	 */
-	ObjectMapper mapper;
+	private final HttpClient httpClient;
+	
+	/**
+	 * The URI string that already contains String interpolation symbols.
+	 */
+	private static final String randomFactURI = "https://catfact.ninja/fact?max_length=%d";
 	
 	public DemoService() {
-		this.mapper = new ObjectMapper();
+		this.httpClient = HttpClient.newHttpClient();
 	}
 	
 	/**
-	 * Turns a json String into a {@code CatFact} object using Jackson2.
-	 * @param json the input json {@code String}
-	 * @return the parsed CatFact object
-	 * @throws JsonMappingException if there is an error with parsing by Jackson2
-	 * @throws JsonProcessingException if there is an error with parsing by Jackson2
+	 * Retrives a random cat fact from Postman's free API.
+	 * @param maxLength maximum length of the fact string
+	 * @return a new @{code CatFact} object after parsing the response body with
+	 * {@code Jackson2}
+	 * @throws IOException if an error occured when sending the request
+	 * @throws InterruptedException if an illegal parameter was passed to the 
+	 * internal HttpRequest builder.
 	 */
-	public CatFact toCatFact(String json) throws JsonMappingException, JsonProcessingException{	
-		return mapper.readValue(json, CatFact.class);
+	public CatFact getRandomCatFact(int maxLength) throws IOException, InterruptedException {
+		//Creates a new request
+		HttpRequest req = HttpRequest.newBuilder()
+			      .uri(URI.create(String.format(randomFactURI, maxLength)))
+			      .build();
+		
+		//Gets response with the body as String
+		HttpResponse<String> res = this.httpClient.send(req, BodyHandlers.ofString());
+		
+		//Parses the response body into a CatFact object using JsonUtils
+		return toCatFact(res.body());
 	}
+	
+
 }
