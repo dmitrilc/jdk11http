3bdf205c1e5a20e06021fccb39cd7075a26301a4
dmitrilc <dmitrilc@users.noreply.github.com>
2021-07-28 09:15:32 -0400

    Refactor to re-use the same ObjectMapper instance
    Creating a new ObjectMapper for every method call is exepensive.

--- jdk11http/src/main/java/net/company/services/DemoService.java
+++ jdk11http/src/main/java/net/company/services/DemoService.java
@@ -14,15 +14,24 @@ import net.company.pojos.CatFact;
 public class DemoService {
 	
 	/**
+	 * The ObjectMapper instance should be re-used because it is expensive
+	 * to create. If a method needs a configuration that requires a specialized
+	 * ObjectMapper, the method can use ObjectMapper's copy constructor. 
+	 */
+	ObjectMapper mapper;
+	
+	public DemoService() {
+		this.mapper = new ObjectMapper();
+	}
+	
+	/**
 	 * Turns a json String into a {@code CatFact} object using Jackson2.
 	 * @param json the input json {@code String}
 	 * @return the parsed CatFact object
 	 * @throws JsonMappingException if there is an error with parsing by Jackson2
 	 * @throws JsonProcessingException if there is an error with parsing by Jackson2
 	 */
-	public CatFact toCatFact(String json) throws JsonMappingException, JsonProcessingException{
-		var mapper = new ObjectMapper();
-		
+	public CatFact toCatFact(String json) throws JsonMappingException, JsonProcessingException{	
 		return mapper.readValue(json, CatFact.class);
 	}
 }
