7a3b1ee6028e3a165eb6fa6e88cc455abebd23fd
dmitrilc <dmitrilc@users.noreply.github.com>
2021-07-28 09:49:47 -0400

    Remove usage of this class
    DemoController not longer performs any logic.

--- jdk11http/src/test/java/net/company/controllers/DemoControllerTest.java
+++ jdk11http/src/test/java/net/company/controllers/DemoControllerTest.java
@@ -1,25 +1,10 @@
 package net.company.controllers;
 
-import java.io.IOException;
-
-import org.junit.jupiter.api.Test;
-
 /**
  * Test class for the DemoController
  * @author dmitrilc
  *
  */
 public class DemoControllerTest {
-	
-	private final DemoController controller = new DemoController(); 
-	
-	/**
-	 * Tests the randomCatFact method.
-	 * @throws IOException if an error occured while sending the request
-	 * @throws InterruptedException if an illegal parameter was passed to theinternal HttpRequest builder.
-	 */
-	@Test
-	public void getRandomCatFactTest() throws IOException, InterruptedException {
-		controller.getRandomCatFact(40);
-	}
+
 }
